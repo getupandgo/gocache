5529d3fd475072c01f2fe522061d5ef4bdcf07b7
getupandgo <getupandgo11@gmail.com>
2019-03-11 16:07:38 +0200

    added endpoints for top pages and removing

--- controllers/pagecache/pagecache.go
+++ controllers/pagecache/pagecache.go
@@ -3,18 +3,27 @@ package pagecache
 import (
 	"github.com/getupandgo/gocache/utils/cache"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type CacheController struct {
 	cacheClient *cache.RedisClient
 }
 
-func Init(cc *cache.RedisClient) CacheController {
-	return CacheController{cc}
+func Init(cc *cache.RedisClient) *CacheController {
+	return &CacheController{cc}
 }
 
-func (ctrl CacheController) GetPage(c *gin.Context) {
+func (ctrl *CacheController) GetPage(c *gin.Context) {
+	ctrl.cacheClient.Test()
+	c.String(http.StatusOK, "qwe")
 }
 
-func (ctrl CacheController) UpsertPage(c *gin.Context) {
+func (ctrl *CacheController) UpsertPage(c *gin.Context) {
+}
+
+func (ctrl *CacheController) DeletePage(c *gin.Context) {
+}
+
+func (ctrl *CacheController) GetTopPages(c *gin.Context) {
 }
--- controllers/router.go
+++ controllers/router.go
@@ -10,9 +10,14 @@ func InitRouter(cc *cache.RedisClient) *gin.Engine {
 	cacheCtrl := pagecache.Init(cc)
 
 	r := gin.New()
+
 	cacheRouter := r.Group("/cache")
 	cacheRouter.GET("", cacheCtrl.GetPage)
 	cacheRouter.PUT("", cacheCtrl.UpsertPage)
+	cacheRouter.DELETE("", cacheCtrl.DeletePage)
+
+	topRouter := r.Group("/top")
+	topRouter.GET("", cacheCtrl.GetTopPages)
 
 	return r
 }

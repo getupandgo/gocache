24dbca7cc5bc9e5c4136b9fc7eb574d26b0355cb
getupandgo <getupandgo11@gmail.com>
2019-03-12 12:35:17 +0200

    test for page creation

--- server/controllers/pagecache/pagecache_test.go
+++ server/controllers/pagecache/pagecache_test.go
@@ -1,5 +1,31 @@
-package pagecache
+package pagecache_test
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/getupandgo/gocache/mocks"
+	"github.com/getupandgo/gocache/server/controllers"
+	"github.com/getupandgo/gocache/server/controllers/pagecache"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
+func TestPageUpsert(t *testing.T) {
+	body, _ := json.Marshal(pagecache.SavePageRequest{"http://example.com"})
+
+	request, _ := http.NewRequest("PUT", "/cache", bytes.NewBuffer(body))
+	response := httptest.NewRecorder()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cacheMock := cache_mock.NewMockCacheClient(ctrl)
+	cacheMock.EXPECT().UpsertPage(gomock.Any()).Return(nil)
+
+	controllers.InitRouter(cacheMock).ServeHTTP(response, request)
+
+	assert.Equal(t, 200, response.Code, "Ok is expected")
 }

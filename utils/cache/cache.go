c43f78903c547a2a693159d56109e021b2389d7e
getupandgo <getupandgo11@gmail.com>
2019-03-12 12:29:34 +0200

    moved to caching interface, implemented page inserting

--- utils/cache/cache.go
+++ utils/cache/cache.go
@@ -5,18 +5,53 @@ import (
 	"github.com/spf13/viper"
 )
 
-type RedisClient struct {
-	redisClient *redis.Client
-}
+type (
+	CacheClient interface {
+		UpsertPage(pg *PageMsg) error
+	}
 
-func (cc *RedisClient) Init(conf *viper.Viper) {
-	options := map[string]string{
+	RedisClient struct {
+		redisClient *redis.Client
+	}
+
+	PageMsg struct {
+		URL         string
+		PageContent string
+	}
+)
+
+func Init(conf *viper.Viper) CacheClient {
+	opts := map[string]string{
 		"host": conf.GetString("redis.host"),
 		"port": conf.GetString("redis.port"),
 	}
 
-	cc.redisClient = redis.NewClient(&redis.Options{
-		Addr: options["host"] + ":" + options["port"],
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: opts["host"] + ":" + opts["port"],
 		DB:   0,
 	})
+
+	return &RedisClient{redisClient}
+}
+
+func (cc *RedisClient) UpsertPage(pg *PageMsg) error {
+	return cc.redisClient.Watch(func(tx *redis.Tx) error {
+		_, err := tx.Pipelined(
+			func(pipe redis.Pipeliner) error {
+				pipe.HSet(pg.URL, "content", pg.PageContent)
+
+				pipe.ZIncr("hits", redis.Z{
+					Score:  1,
+					Member: pg.URL,
+				})
+
+				pipe.ZIncr("ttl", redis.Z{
+					Score:  1,
+					Member: pg.URL,
+				})
+
+				return nil
+			})
+		return err
+	}, pg.URL)
 }

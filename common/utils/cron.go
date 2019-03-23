ea1d940d1cb4702e07d9c9d69a6781feb4a038fd
getupandgo <getupandgo11@gmail.com>
2019-03-23 20:27:53 +0200

    refactored expiration job, added polling interval

--- common/utils/cron.go
+++ common/utils/cron.go
@@ -1,26 +1,31 @@
 package utils
 
 import (
+	"time"
+
 	"github.com/getupandgo/gocache/common/cache"
-	"github.com/robfig/cron"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
-func WatchExpiredRecords(cc cache.CacheClient) error {
-	c := cron.New()
-	err := c.AddFunc("@every 1m", func() {
-		if _, err := cc.RemoveExpiredRecords(); err != nil {
-			log.Info().
-				Err(err).
-				Msg("Failed to delete expired records")
+func WatchExpiredRecords(cc cache.Page) error {
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+		for t := range ticker.C {
+			_ = t
+
+			if _, err := cc.Expire(); err != nil {
+				log.Info().
+					Err(err).
+					Msg("Failed to delete expired records")
+			}
 		}
-	})
+	}()
 
-	if err != nil {
-		return err
-	}
-
-	c.Start()
+	pollInterv := viper.GetInt64("limits.expire_interval")
+	time.Sleep(10 * time.Duration(pollInterv))
+	ticker.Stop()
 
 	return nil
 }
